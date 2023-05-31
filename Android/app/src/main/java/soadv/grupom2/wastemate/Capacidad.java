32402f3ab88f004c1822552786a3e6d79e189511
AgustinCocciardi <agustincocciardi@outlook.com>
2023-05-31 10:01:46 -0300

    Muestra el porcentaje de llenado del tacho (valores hardcodeados)

--- Android/app/src/main/java/soadv/grupom2/wastemate/Capacidad.java
+++ Android/app/src/main/java/soadv/grupom2/wastemate/Capacidad.java
@@ -2,13 +2,26 @@ package soadv.grupom2.wastemate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class Capacidad extends AppCompatActivity {
 
+    private double capacidadReal = 80;
+    private final static double capacidadMaxima = 150;
+    private TextView capacidadText;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capacidad);
+        capacidadText = findViewById(R.id.mostrarCapacidad);
+        //capacidadText.setText(Double.toString(capacidadReal/capacidadMaxima));
+        DecimalFormat formatoDecimal = new DecimalFormat("#.00");
+        capacidadText.setText(Double.toString(Double.parseDouble(formatoDecimal.format(capacidadReal/capacidadMaxima))*100)+" %");
     }
 }
