6d0200e629ddc685f5bffe1d005ed70e1737cd40
AgustinCocciardi <agustincocciardi@outlook.com>
2023-06-03 20:11:12 -0300

    DeviceListActivity y DeviceListAdapter

---
+++ Android/app/src/main/java/soadv/grupom2/wastemate/DeviceListAdapter.java
@@ -0,0 +1,14 @@
+package soadv.grupom2.wastemate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeviceListAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item_device);
+    }
+}
